c3aeec48e5f010cf20c74131254e686fc6045960
prismistim <prismistim@hotmail.com>
2021-11-14 17:43:57 +0900

    init: ESLint系の環境構築

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+  },
+}
