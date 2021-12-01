d26c20c308cf5832b6dc9e334b3a35eb3050218f
snowsphere <prismistim@hotmail.com>
2021-12-01 21:11:17 +0900

    tailwindcssとstorybookの追加 (#5)
    * feat: tailwindの導入とサンプルコンポーネントの作成
    
    * feat: storybookの追加

---
+++ .storybook/main.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss')
+        }
+      }
+    }
+  ],
+  framework: '@storybook/vue3',
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    })
+
+    // Return the altered config
+    return config
+  }
+}
---
+++ src/components/atoms/BtnDefault.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+    required: true
+  }
+})
+</script>
+
+<template>
+  <button class="px-4 py-2 rounded-md bg-indigo-600 text-white">
+    {{ props.title }}
+  </button>
+</template>
+
+<style lang="css">
+.button {
+  @apply px-4 py-2 rounded-md bg-indigo-600 text-white;
+}
+</style>
