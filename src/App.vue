95afd44fb71bebd24ea9c0b977afa83fffa1140b
prismistim <prismistim@hotmail.com>
2021-12-04 18:05:18 +0900

    feat: レイアウトに余白設定を追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import TemplateDefault from '/@/components/templates/TemplateDefault.vue'
-</script>
-
 <template>
-  <template-default>
-    <template #content>
-      <router-view />
-    </template>
-  </template-default>
+  <router-view />
 </template>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import TemplateDefault from '/@/components/templates/TemplateDefault.vue'
 import HelloWorld from '/@/components/organisms/home/HelloWorld.vue'
 import BtnDefault from '/@/components/atoms/BtnDefault.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="/@/assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <btn-default title="hogehoge" />
+  <template-default>
+    <template #page-title> home </template>
+    <template #content>
+      <img alt="Vue logo" src="/@/assets/logo.png" />
+      <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      <btn-default title="hogehoge" />
+    </template>
+  </template-default>
 </template>
