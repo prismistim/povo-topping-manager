aba39854e2755a4febe4196a368190f3d007ef2f
prismistim <prismistim@hotmail.com>
2021-12-04 17:44:43 +0900

    feat: 仮レイアウトデザインを追加

---
+++ src/components/templates/TemplateDefault.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot name="page-title" />
+    <div class="border-b border-gray-400" />
+    <div class="container mx-auto px-10 md:px-60">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style lang="postcss">
+@import '../../assets/index.css';
+</style>
