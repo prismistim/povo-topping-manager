10194f8ef2d4f02e1a15dc999e43cb5d47e5e78d
prismistim <prismistim@hotmail.com>
2021-12-04 16:31:47 +0900

    change: storybookのbuilderをviteに変更

--- src/components/atoms/BtnDefault.vue
+++ src/components/atoms/BtnDefault.vue
@@ -14,8 +14,6 @@ const props = defineProps({
   </button>
 </template>
 
-<style lang="css">
-.button {
-  @apply px-4 py-2 rounded-md bg-indigo-600 text-white;
-}
+<style lang="postcss" scoped>
+@import '../../assets/index.css';
 </style>
