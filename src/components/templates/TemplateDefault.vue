2e91df9cfbf16732f26a7b9f6082bafde0eefb40
snowsphere <prismistim@hotmail.com>
2021-12-04 18:07:24 +0900

    feat: レイアウトに余白設定を追加 (#17)

--- src/components/templates/TemplateDefault.vue
+++ src/components/templates/TemplateDefault.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <slot name="page-title" />
-    <div class="border-b border-gray-400" />
-    <div class="container mx-auto px-10 md:px-60">
+  <div class="container mx-auto px-10 md:px-60">
+    <div class="mt-16 mb-10 text-center">
+      <slot name="page-title" />
+    </div>
+    <div class="mb-10 border-b border-gray-400" />
+    <div>
       <slot name="content" />
     </div>
   </div>
