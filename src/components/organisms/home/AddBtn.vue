80d606634b14930f13b0e41135386ba97d800318
Shota Tanaka <perblue.666@gmail.com>
2021-12-08 18:04:13 +0900

    feat: 新規登録のボタンと購入したトッッピングのリストのデザインベース作成

---
+++ src/components/organisms/home/AddBtn.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="text-center my-10">
+    <button
+      class="w-40 h-40 bg-yellow-300 text-lg rounded-full hover:bg-yellow-400 shadow-md font-mono"
+    >
+      新規登録
+    </button>
+  </div>
+</template>
---
+++ src/components/organisms/home/ToppingListHistory.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  toppingName: String,
+  purchasedDate: Date
+})
+</script>
+
+<template>
+  <div class="pt-10">
+    <p class="font-mono text-gray-500">購入したトッッピング</p>
+    <ul>
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-4 my-4 shadow border-l-4 border-yellow-300"
+          >HRとは</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-4 my-4 shadow border-l-4 border-yellow-300"
+          >HRとは</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
