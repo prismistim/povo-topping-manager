22eeb709761aba15c2eca912e44df8cbd16d6298
ShoContain <perblue.666@gmail.com>
2021-12-22 00:19:33 +0900

    change:トッピングの型を親子間で共有

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
 import TemplateDefault from '/@/components/templates/TemplateDefault.vue'
 import AddBtn from '/@/components/organisms/home/AddBtn.vue'
-import ToppingListHistory from '/@/components/organisms/home/ToppingListHistory.vue'
+import ToppingListHistory, {
+  Props
+} from '/@/components/organisms/home/ToppingListHistory.vue'
+
+const data: Props[] = [
+  {
+    id: 1,
+    toppingName: 'データ追加３GB（30日間）',
+    purchasedDate: '2021-12-12',
+    remainingDays: 8
+  },
+  {
+    id: 2,
+    toppingName: 'データ追加5GB（7日間）',
+    purchasedDate: '2021-12-14',
+    remainingDays: 2
+  }
+]
 </script>
 
 <template>
@@ -9,7 +26,7 @@ import ToppingListHistory from '/@/components/organisms/home/ToppingListHistory.
     <template #page-title> povo-topping-manager </template>
     <template #content>
       <AddBtn />
-      <ToppingListHistory topping-name="hoge" purchased-date="2021-12-23" />
+      <ToppingListHistory :data="data" />
     </template>
   </template-default>
 </template>
