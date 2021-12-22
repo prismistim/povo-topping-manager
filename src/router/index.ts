70da330dcb5cdaa03f165b10f0a2e65ef32a0023
Shota Tanaka <perblue.666@gmail.com>
2021-12-22 18:10:46 +0900

    feat: どのパスにもマッチングしない場合のルート設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '/@/components/pages/Home.vue'
+import NotFound from '/@/components/pages/NotFound.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
