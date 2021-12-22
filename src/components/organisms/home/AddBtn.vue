16f77bef1fa384a48855e01ee41d50d0bfcc6e14
prismistim <info@snowsphere.net>
2021-12-22 12:08:51 +0000

    feat: リンクにslashを追加

--- src/components/organisms/home/AddBtn.vue
+++ src/components/organisms/home/AddBtn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center my-10">
-    <router-link to="add">
+    <router-link to="/add">
       <button
         class="w-40 h-40 bg-povo hover:bg-yellow-100 text-lg rounded-full shadow-md font-mono"
       >
