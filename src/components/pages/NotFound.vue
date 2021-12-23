910ff5be3cd1c068d5a47e65ad4135dc2df8ac3b
Shota Tanaka <perblue.666@gmail.com>
2021-12-23 17:25:37 +0900

    change:404のフォント位置調整

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,25 +1,19 @@
 <template>
   <main class="flex items-center justify-center h-screen text-gray-500">
     <div class="p-4 space-y-4">
-      <div
-        class="flex items-start space-y-3 sm:flex-row sm:space-y-0 sm:items-center sm:space-x-3"
-      >
-        <p class="font-semibold text-red-500 text-9xl">404</p>
-        <div class="space-y-2">
-          <h1 class="flex items-center space-x-2">
-            <span class="text-xl font-medium text-gray-600 sm:text-2xl">
-              お探しのページが見つかりません。
-            </span>
-          </h1>
-          <p class="text-base font-normal text-gray-500">
-            ホーム画面に戻る際は
-            <router-link to="/" class="text-blue-600 hover:underline"
-              >ホーム</router-link
-            >
-            をクリックして下さい
-          </p>
-        </div>
-      </div>
+      <p class="font-semibold text-red-500 text-9xl text-center">404</p>
+      <h1 class="space-x-2">
+        <span class="text-xl font-medium text-gray-600 sm:text-2xl">
+          お探しのページが見つかりません。
+        </span>
+      </h1>
+      <p class="text-base font-normal text-gray-500">
+        ホーム画面に戻る際は
+        <router-link to="/" class="text-blue-600 hover:underline"
+          >ホーム</router-link
+        >
+        をクリックして下さい
+      </p>
     </div>
   </main>
 </template>
