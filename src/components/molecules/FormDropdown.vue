d5d60cf423d7a54213c6153e4be559eb55cecac7
ShoContain <perblue.666@gmail.com>
2022-01-11 14:32:09 +0900

    feature: dropdownコンポーネント作成

---
+++ src/components/molecules/FormDropdown.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+import FormLabelVue from '/@/components/atoms/FormLabel.vue'
+export type Props = {
+  key: number
+  value: number
+  label: string
+}
+const props = defineProps({
+  options: {
+    type: Array as PropType<Props[]>,
+    default: () => [],
+    required: false
+  }
+})
+</script>
+
+<template>
+  <FormLabelVue text="トッピング"></FormLabelVue>
+  <select id="" name="">
+    <option
+      v-for="option in props.options"
+      :key="option.key ?? option"
+      :value="option.value ?? option"
+    >
+      {{ option.label ?? option }}
+    </option>
+  </select>
+</template>
--- src/components/pages/Add.vue
+++ src/components/pages/Add.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import TemplateDefault from '/@/components/templates/TemplateDefault.vue'
+import FormDropdown from '/@/components/molecules/FormDropdown.vue'
+
+const selectOptions = [
+  { key: 1, value: 1, label: 'データ使い放題 (24時間）' },
+  { key: 2, value: 7, label: 'データ追加1GB（7日間）' },
+  { key: 3, value: 30, label: 'データ追加3GB（30日間）' },
+  { key: 4, value: 30, label: 'データ追加20GB（30日間）' },
+  { key: 5, value: 90, label: 'データ追加60GB（90日間）' },
+  { key: 6, value: 180, label: 'データ追加150GB（180日間）' }
+]
 </script>
 
 <template>
   <template-default :has-back-logo="true">
     <template #page-title> 購入を登録する </template>
-    <template #content> Add </template>
+    <template #content>
+      <FormDropdown :options="selectOptions"></FormDropdown>
+    </template>
   </template-default>
 </template>
