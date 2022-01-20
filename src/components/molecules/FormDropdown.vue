6476d88e957787124f19d782df4caf8a3e8571b9
ShoContain <perblue.666@gmail.com>
2022-01-20 17:14:15 +0900

    change: dropdownで選択が変更されたときのemitを定義

--- src/components/molecules/FormDropdown.vue
+++ src/components/molecules/FormDropdown.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { PropType } from 'vue'
-import FormLabelVue from '/@/components/atoms/FormLabel.vue'
+import { PropType, computed } from 'vue'
 
 type Option = {
-  key: number
-  value: number
-  label: string
+  key: string | number
+  label: string | number
+  value?: string | number
+  remainingDays?: string | number
 }
 
 const props = defineProps({
@@ -14,17 +14,26 @@ const props = defineProps({
     default: () => [],
     required: false
   },
-  text: {
-    type: String,
-    default: '',
-    required: false
+  modelValue: {
+    type: [String, Number, Boolean, Array, Object],
+    default: ''
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+const computedValue = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+    emit('update:modelValue', value)
   }
 })
 </script>
 
 <template>
-  <FormLabelVue :text="props.text"></FormLabelVue>
-  <select class="w-full px-2 py-1 mb-5 border-2 rounded-md">
+  <select
+    v-model="computedValue"
+    class="w-full px-2 py-1 mb-5 border-2 rounded-md"
+  >
     <option
       v-for="option in props.options"
       :key="option.key ?? option"
